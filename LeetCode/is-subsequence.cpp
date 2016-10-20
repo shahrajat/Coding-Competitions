76c624129a8b4a6a25d99fce4559a0d7c8d41052
Rajat Shah <rajat.shah200792@gmail.com>
2016-10-20 14:50:33 -0400

    Create is-subsequence.cpp

---
+++ LeetCode/is-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for(int i=0; i<t.length() && j < s.length(); i++) {
+            if(t[i]==s[j])
+                j++;
+        }
+        if(j==s.length())
+            return true;
+        return false;
+    }
+};
